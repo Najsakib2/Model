ab7f1f51d4e50549429c58ff07d775ea590ea49e
NAZMUS SAKIB <sakibnajsakib@gmail.com>
2021-09-06 19:18:04 +0600

    ok

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-model');
+const btnOpenModal = document.querySelector('.show-model');
+console.log(btnOpenModal);
