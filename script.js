d3012dc0aec2218872ebfd23f3219ae0d7e9abc5
NAZMUS SAKIB <sakibnajsakib@gmail.com>
2021-09-07 12:05:17 +0600

    ok

--- script.js
+++ script.js
@@ -3,5 +3,11 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-model');
-const btnOpenModal = document.querySelector('.show-model');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
+for (let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener('click', function () {
+        console.log('Button Clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
